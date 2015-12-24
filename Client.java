8dec21b5d6a8185d43895abdf7e11297ffd7af8a
wwolfyTC <tim011919@gmail.com>
2015-12-24 21:32:35 +0800

    complete client GUI

--- Client.java
+++ Client.java
@@ -14,80 +14,166 @@ public class Client{
 		setupUI();
 	}
 
-	private void setupUI(){
-		frame = new JFrame();
-		frame.setSize(600, 400);
-		frame.setLayout(new GridBagLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		GridBagConstraints cons = new GridBagConstraints();
-		cons.insets = new Insets(2, 2, 2, 2);
-
-		JLabel ipL = new JLabel("server IP: ");
-		cons.gridx = 0;		cons.gridy = 0;
-        cons.gridwidth = 1;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.NONE;
-        cons.anchor = GridBagConstraints.CENTER;
-        frame.add(ipL, cons);
-
-        ipT = new JTextField("", 15);
-        ipT.setMinimumSize(ipT.getPreferredSize());
-		cons.gridx = 1;		cons.gridy = 0;
-        cons.gridwidth = 2;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.NONE;
-        cons.anchor = GridBagConstraints.WEST;
-        frame.add(ipT, cons);
-
-        JLabel portL = new JLabel("port: ");
-		cons.gridx = 3;		cons.gridy = 0;
-        cons.gridwidth = 1;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.NONE;
-        cons.anchor = GridBagConstraints.CENTER;
-        frame.add(portL, cons);
-
-        portT = new JTextField("", 6);
-		cons.gridx = 4;		cons.gridy = 0;
-        cons.gridwidth = 1;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.NONE;
-        cons.anchor = GridBagConstraints.WEST;
-        frame.add(portT, cons);
-
-        JButton connectB = new JButton("connect");
-        //connectB.addActionListener(new connectListener());
-		cons.gridx = 5;		cons.gridy = 0;
-        cons.gridwidth = 1;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.CENTER;
-        cons.anchor = GridBagConstraints.CENTER;
-        frame.add(connectB, cons);
-
-        info = new JLabel("welcome!");
-		cons.gridx = 0;		cons.gridy = 1;
-        cons.gridwidth = 9;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.HORIZONTAL;
-        cons.anchor = GridBagConstraints.WEST;
-        frame.add(info, cons);
-
-        ////
-
-        JButton uploadB = new JButton("upload>");
-        //
-        cons.gridx = 2;		cons.gridy = 2;
-        cons.gridwidth = 1;	cons.gridheight = 1;
-        cons.weightx = 0;	cons.weighty = 0;
-        cons.fill = GridBagConstraints.BOTH;
-        cons.anchor = GridBagConstraints.CENTER;
-        frame.add(uploadB, cons);
-
-        frame.setVisible(true);
+	private void printMsg(String str){
+		
 	}
 
+	private void setupUI(){
+		frame = new JFrame();
+		frame.setLayout(new GridBagLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.setResizable(false);
+		
+		GridBagConstraints cons = new GridBagConstraints();
+		cons.insets = new Insets(4, 4, 4, 4);
+		
+		/***row 1***/
+		JLabel ipL = new JLabel("server IP: ");
+		cons.gridx = 0;		cons.gridy = 0;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.NONE;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(ipL, cons);
+		
+		ipTextField = new JTextField(20);
+		cons.gridx = 1;		cons.gridy = 0;
+		cons.gridwidth = 2;	cons.gridheight = 1;
+		cons.weightx = 1.0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.HORIZONTAL;
+		cons.anchor = GridBagConstraints.WEST;
+		frame.add(ipTextField, cons);
+		
+		JLabel portL = new JLabel("port: ");
+		cons.gridx = 3;		cons.gridy = 0;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.NONE;
+		cons.anchor = GridBagConstraints.EAST;
+		frame.add(portL, cons);
+		
+		portTextField = new JTextField(6);
+		cons.gridx = 4;		cons.gridy = 0;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.NONE;
+		cons.anchor = GridBagConstraints.WEST;
+		frame.add(portTextField, cons);
+		
+		JButton connectB = new JButton("Connect");
+		//connectB.addActionListener(new connectListener());
+		cons.gridx = 5;		cons.gridy = 0;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(connectB, cons);
+		
+
+		/***row 1***/
+		info = new JLabel("welcome!");
+		cons.gridx = 0;		cons.gridy = 1;
+		cons.gridwidth = 6;	cons.gridheight = 1;
+		cons.weightx = 1.0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.HORIZONTAL;
+		cons.anchor = GridBagConstraints.WEST;
+		frame.add(info, cons);
+
+		/***row 2***/
+		JLabel fileL = new JLabel("Local file:");
+		cons.gridx = 0;		cons.gridy = 2;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.NONE;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(fileL, cons);
+
+		selectedFileTextField = new JTextField(30);
+		cons.gridx = 1;		cons.gridy = 2;
+		cons.gridwidth = 4;	cons.gridheight = 1;
+		cons.weightx = 1.0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.HORIZONTAL;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(selectedFileTextField, cons);
+
+		JButton browseB = new JButton("Browse");
+		//
+		cons.gridx = 5;		cons.gridy = 2;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(browseB, cons);
+
+		/***row 3 and below***/
+		/*server file list*/
+		DefaultListModel<String> lis = new DefaultListModel<String>();
+		lis.addElement("disconnected");
+
+		serverFileList = new JList<String>(lis);
+		serverFileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		serverFileList.setFont(new Font("", Font.ITALIC, 12));
+		//serverFileList.setVisibleRowCount(7);
+		//list.addListListener()
+
+		JScrollPane pane = new JScrollPane(serverFileList);
+		cons.gridx = 0;		cons.gridy = 3;
+		cons.gridwidth = 5;	cons.gridheight = 4;
+		cons.weightx = 1.0;	cons.weighty = 1.0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(pane, cons);
+		/*end server file list*/
+
+		
+		JButton uploadB = new JButton("Upload");
+		//
+		uploadB.setEnabled(false);
+		cons.gridx = 5;		cons.gridy = 3;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 1.0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(uploadB, cons);
+
+		JButton downloadB = new JButton("Download");
+		//
+		downloadB.setEnabled(false);
+		cons.gridx = 5;		cons.gridy = 4;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 1.0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(downloadB, cons);
+
+		JButton renameB = new JButton("Rename");
+		//
+		renameB.setEnabled(false);
+		cons.gridx = 5;		cons.gridy = 5;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 1.0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(renameB, cons);
+
+		JButton deleteB = new JButton("Delete");
+		//
+		deleteB.setEnabled(false);
+		cons.gridx = 5;		cons.gridy = 6;
+		cons.gridwidth = 1;	cons.gridheight = 1;
+		cons.weightx = 0;	cons.weighty = 1.0;
+		cons.fill = GridBagConstraints.BOTH;
+		cons.anchor = GridBagConstraints.CENTER;
+		frame.add(deleteB, cons);
+
+		/***end element setting**/
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 	private JFrame frame;
 	private JLabel info;
-	private JTextField ipT, portT;
+	private JTextField ipTextField, portTextField, selectedFileTextField;
+	private JList<String> serverFileList;
 }
