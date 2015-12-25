f7c38dc5285531409ec9ac37389d54389e0ed749
wwolfyTC <tim011919@gmail.com>
2015-12-25 17:02:36 +0800

    server

--- Server.java
+++ Server.java
@@ -6,8 +6,27 @@ import ezprivacy.service.authsocket.*;
 import ezprivacy.secret.*;
 import ezprivacy.netty.session.ProtocolException;
 
-class Server{
+public class Server
+	implements Runnable
+{
 	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+
+		Server svr = new Server();
+		svr.run();
+	}
+
+	public void run(){
+		System.out.print("port number: ");
+		port = scan.nextInt();
+
 		
 	}
+
+	private Scanner scan;
+
+	private ServerSocket ss;
+	private Socket sock;
+	private String clientIP;
+	private int port;
 }
